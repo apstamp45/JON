c4a647697a0a6e094af137d1280c0b2355170b2f
Arnon Oldja <apstamp45@pop-os.localdomain>
2021-08-06 20:09:47 -0400

    First test.

---
+++ src/main/java/com/apstamp45/JON/App.java
@@ -0,0 +1,13 @@
+package com.apstamp45.JON;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/apstamp45/JON/Main.java
@@ -0,0 +1,17 @@
+/**
+ * This project attempts to create a
+ * JavaScript-like object layout in
+ * the Java language. It is simply
+ * a fun practice project.
+ * @author apstamp45
+ */
+
+package com.apstamp45.JON;
+/**
+ * Main class.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
